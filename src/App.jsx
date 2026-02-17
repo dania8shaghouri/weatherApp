4e187dc14d3f05c6c02dced44bb7a311b0dbdb6a
dania8shaghouri <daniashaghouri@gmail.com>
2026-02-17 14:50:40 +0300

    added loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,141 @@
+// import { useTranslation } from "react-i18next";
+// import { useEffect, useState } from "react";
+
+// // import axios from "axios";
+// import moment from "moment";
+// import "moment/locale/ar";
+
+// import "./App.css";
+// import "./index.css";
+// import WeatherCard from "./components/WeatherCard";
+
+// import { useSelector, useDispatch } from "react-redux";
+// import { changeResult } from "./weatherApiSlice";
+// import { fetchWeather } from "./weatherApiSlice";
+// // let cancelAxios = null;
+
+// function App() {
+//   // redux code
+//   const dispath = useDispatch();
+//   const result = useSelector((state) => {
+//     console.log("the state is ", state);
+//     return state.result;
+//   });
+
+//   const isLoading = useSelector((state) => {
+//     return state.weather.isLoading;
+//   });
+
+//   const weather = useSelector((state) => state.weather);
+
+//   const error = useSelector((state) => state.error);
+
+//   const [dateAndTime, setdateAndTime] = useState("");
+//   const [temp, setTemp] = useState({
+//     number: null,
+//     description: "",
+//     min: null,
+//     max: null,
+//     icon: null,
+//   });
+//   const { t, i18n } = useTranslation();
+
+//   useEffect(() => {
+//     // trying redux
+//     // dispath(changeResult())
+
+//     dispath(fetchWeather());
+//     moment.locale(i18n.language);
+//     setdateAndTime(moment().format("MMM Do YYYY, h:mm:ss a"));
+//     document.documentElement.dir = i18n.language === "ar" ? "rtl" : "ltr";
+//   }, [i18n.language]);
+
+//   const API_KEY = import.meta.env.VITE_WEATHER_KEY;
+//   // console.log("KEY:", import.meta.env.VITE_WEATHER_KEY);
+
+//   // useEffect(() => {
+//   //   axios
+//   //     .get(
+//   //       `https://api.openweathermap.org/data/2.5/weather?lat=40.19&lon=29.07&appid=${API_KEY}`,
+//   //     )
+//   //     .then(function (response) {
+//   //       // 272.15 kelvin to celsius
+//   //       const responseTemp = Math.round(response.data.main.temp - 272.15);
+//   //       const min = Math.round(response.data.main.temp_min - 272.15);
+//   //       const max = Math.round(response.data.main.temp_max - 272.15);
+//   //       const description = response.data.weather[0].description;
+//   //       const responseIcon = response.data.weather[0].icon;
+
+//   //       setTemp({
+//   //         number: responseTemp,
+//   //         min: min,
+//   //         max: max,
+//   //         description: description,
+//   //         icon: `https://openweathermap.org/img/wn/${responseIcon}@2x.png`,
+//   //       });
+//   //       console.log(response);
+//   //     })
+//   //     .catch(function (error) {
+//   //       console.log(error);
+//   //     });
+//   // }, []);
+
+//   return (
+//     <>
+//       <div
+//         className="bg-[var(--body-bg)] 
+//     min-h-screen flex flex-col items-center justify-center gap-4 "
+//       >
+//         {/* dil buttonu */}
+//         <button
+//           onClick={() =>
+//             i18n.changeLanguage(i18n.language === "en" ? "ar" : "en")
+//           }
+//           className="px-4 py-1 border rounded"
+//         >
+//           {i18n.language === "en" ? "Arabic" : "English"}
+//         </button>
+
+//         {/* weahter card */}
+//         {/* <WeatherCard
+//           city={t("city")}
+//           date={dateAndTime}
+//           condition={temp.description}
+//           icon={temp.icon}
+//           temp={temp.number}
+//           high={temp.max}
+//           low={temp.min}
+//         /> */}
+//         {isLoading && <p>Loading...</p>}
+
+//         {error && <p>{error}</p>}
+
+//         {weather && (
+//           <WeatherCard
+//             city={t("city")}
+//             date={dateAndTime}
+//             condition={weather.description}
+//             icon={weather.icon}
+//             temp={weather.number}
+//             high={weather.max}
+//             low={weather.min}
+//           />
+//         )}
+//       </div>
+//     </>
+//   );
+// }
+
+// export default App;
+
+
+
+
+
 import { useTranslation } from "react-i18next";
 import { useEffect, useState } from "react";
 
-import axios from "axios";
+// import axios from "axios";
 import moment from "moment";
 import "moment/locale/ar";
 
@@ -11,64 +145,49 @@ import WeatherCard from "./components/WeatherCard";
 
 import { useSelector, useDispatch } from "react-redux";
 import { changeResult } from "./weatherApiSlice";
-// let cancelAxios = null;
+import { fetchWeather } from "./weatherApiSlice";
+// REDUX IMPORT
+
+
+
+let cancelAxios = null;
 
 function App() {
-  // redux code
-  const dispath = useDispatch();
-  const result = useSelector((state) => {
-    console.log("the state is ", state);
-    return state.result;
-  });
-  const [dateAndTime, setdateAndTime] = useState("");
-  const [temp, setTemp] = useState({
-    number: null,
-    description: "",
-    min: null,
-    max: null,
-    icon: null,
-  });
-  const { t, i18n } = useTranslation();
+	// REDUX CODE
+	const dispatch = useDispatch();
 
-  useEffect(() => {
-    // trying redux
-    dispath(changeResult())
-    moment.locale(i18n.language);
-    setdateAndTime(moment().format("MMM Do YYYY, h:mm:ss a"));
-    document.documentElement.dir = i18n.language === "ar" ? "rtl" : "ltr";
-  }, [i18n.language]);
+	const isLoading = useSelector((state) => {
+		console.log("============");
+		console.log(state);
+		return state.weather.isLoading;
+	});
 
-  const API_KEY = import.meta.env.VITE_WEATHER_KEY;
-  // console.log("KEY:", import.meta.env.VITE_WEATHER_KEY);
+	const temp = useSelector((state) => {
+		return state.weather.weather;
+	});
 
-  useEffect(() => {
-    axios
-      .get(
-        `https://api.openweathermap.org/data/2.5/weather?lat=40.19&lon=29.07&appid=${API_KEY}`,
-      )
-      .then(function (response) {
-        // 272.15 kelvin to celsius
-        const responseTemp = Math.round(response.data.main.temp - 272.15);
-        const min = Math.round(response.data.main.temp_min - 272.15);
-        const max = Math.round(response.data.main.temp_max - 272.15);
-        const description = response.data.weather[0].description;
-        const responseIcon = response.data.weather[0].icon;
+	const { t, i18n } = useTranslation();
 
-        setTemp({
-          number: responseTemp,
-          min: min,
-          max: max,
-          description: description,
-          icon: `https://openweathermap.org/img/wn/${responseIcon}@2x.png`,
-        });
-        console.log(response);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  }, []);
+	// ======== STATES ========= //
+	const [dateAndTime, setDateAndTime] = useState("");
+const error = useSelector((state) => state.weather.error);
 
-  return (
+	
+const locale = i18n.language;
+
+	useEffect(() => {
+		// trying redux
+		// dispatch(changeResult());
+
+		console.log("dispatching fetch weather from the component");
+		dispatch(fetchWeather());
+
+		i18n.changeLanguage(locale);
+	}, []);
+	useEffect(() => {
+		setDateAndTime(moment().format("MMMM Do YYYY, h:mm:ss a"));
+	}, []);
+return (
     <>
       <div
         className="bg-[var(--body-bg)] 
@@ -85,7 +204,7 @@ function App() {
         </button>
 
         {/* weahter card */}
-        <WeatherCard
+        {/* <WeatherCard
           city={t("city")}
           date={dateAndTime}
           condition={temp.description}
@@ -93,7 +212,22 @@ function App() {
           temp={temp.number}
           high={temp.max}
           low={temp.min}
-        />
+        /> */}
+        {isLoading && <p>Loading...</p>}
+
+        {error && <p>{error}</p>}
+
+        {temp && (
+          <WeatherCard
+            city={t("city")}
+            date={dateAndTime}
+            condition={temp.description}
+            icon={temp.icon}
+            temp={temp.number}
+            high={temp.max}
+            low={temp.min}
+          />
+        )}
       </div>
     </>
   );
--- src/weatherApiSlice.js
+++ src/weatherApiSlice.js
@@ -1,16 +1,143 @@
-import { createSlice } from "@reduxjs/toolkit";
+// import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+// import axios from "axios";
 
-const wetherApiSlice = createSlice({
+// export const fetchWeather = createAsyncThunk("weatherApi/fetchWeather",
+//   async () => {
+//     try {
+//   const response = await axios.get(
+//     `https://api.openweathermap.org/data/2.5/weather?lat=40.19&lon=29.07&appid=${API_KEY}`,
+//     // {
+//     //   cancelToken: new cancelToken((c) => {
+//     //     cancelAxios = c;
+//     //   }),
+//     // },
+//   );
+
+//   // 272.15 kelvin to celsius
+//   const responseTemp = Math.round(response.data.main.temp - 272.15);
+//   const min = Math.round(response.data.main.temp_min - 272.15);
+//   const max = Math.round(response.data.main.temp_max - 272.15);
+//   const description = response.data.weather[0].description;
+//   const responseIcon = response.data.weather[0].icon;
+
+//   // setTemp({
+//   //   number: responseTemp,
+//   //   min: min,
+//   //   max: max,
+//   //   description: description,
+//   //   icon: `https://openweathermap.org/img/wn/${responseIcon}@2x.png`,
+//   // });
+// }});
+
+// const wetherApiSlice = createSlice({
+//   name: "weatherApi",
+//   initialState: {
+//     result: "empty",
+//     weather: {},
+//     isLoading:false,
+//     error: null
+
+//   },
+//   reducers: {
+//     changeResult: (state, action) => {
+//       state.result = "changed";
+//     },
+//   },
+
+//   extraReducers(builder){
+//     builder.addCase(fetchWeather.pending, (state, action) => {
+//       state.isLoading = true;
+//       state.error=null;
+
+//     }).addCase(fetchWeather.fulfilled, (state, action) => {
+//       state.isLoading = false;
+//       state.weather = action.payload;
+//     })
+//     .addCase(fetchWeather.rejected, (state, action) => {
+//       state.isLoading= false;
+//       state.error = action.payload
+//     })
+//   }
+// });
+
+// export const { changeResult } = wetherApiSlice.actions;
+// export default wetherApiSlice.reducer;
+
+import axios from "axios";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+const API_KEY = import.meta.env.VITE_WEATHER_KEY;
+export const fetchWeather = createAsyncThunk(
+  "weatherApi/fetchWeather",
+  async () => {
+    console.log("calling fetch weather");
+    
+    const response = await axios.get(
+      
+      `https://api.openweathermap.org/data/2.5/weather?lat=40.19&lon=29.07&appid=${API_KEY}`,
+      // {
+      // 	cancelToken: new axios.CancelToken((c) => {
+      // 		cancelAxios = c;
+      // 	}),
+      // }
+    );
+
+    // handle success
+    const responseTemp = Math.round(response.data.main.temp - 272.15);
+    const min = Math.round(response.data.main.temp_min - 272.15);
+    const max = Math.round(response.data.main.temp_max - 272.15);
+    const description = response.data.weather[0].description;
+    const responseIcon = response.data.weather[0].icon;
+
+    console.log(response);
+    // setTemp({
+    // 	number: responseTemp,
+    // 	min: min,
+    // 	max: max,
+    // 	description: description,
+    // 	icon: `https://openweathermap.org/img/wn/${responseIcon}@2x.png`,
+    // });
+
+    return {
+      number: responseTemp,
+      min,
+      max,
+      description,
+      icon: `https://openweathermap.org/img/wn/${responseIcon}@2x.png`,
+    };
+  },
+);
+const weatherApiSlice = createSlice({
   name: "weatherApi",
+
   initialState: {
     result: "empty",
+    weather: null,
+    isLoading: false,
   },
+
   reducers: {
     changeResult: (state, action) => {
       state.result = "changed";
     },
   },
+
+  extraReducers(builder) {
+    builder
+      .addCase(fetchWeather.pending, (state, action) => {
+        state.isLoading = true;
+      })
+      .addCase(fetchWeather.fulfilled, (state, action) => {
+        state.isLoading = false;
+        console.log("============**********");
+
+        console.log(state, action);
+        state.weather = action.payload;
+      })
+      .addCase(fetchWeather.rejected, (state, action) => {
+        state.isLoading = false;
+      });
+  },
 });
 
-export const { changeResult } = wetherApiSlice.actions;
-export default wetherApiSlice.reducer;
+export const { changeResult } = weatherApiSlice.actions;
+export default weatherApiSlice.reducer;
